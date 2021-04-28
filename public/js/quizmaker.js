562e7f35ca27b415c12114541d10e7902aa19c55
Anthony Castillo <anthonycastillo821@gmail.com>
2021-04-28 13:09:22 -0500

    Syncing with new Main

--- public/js/quizmaker.js
+++ public/js/quizmaker.js
@@ -17,96 +17,8 @@ let answerInputCEl = document.querySelector("#answerchoicec")
 let answerInputDEl = document.querySelector("#answerchoiced")
 let correctRInputEl = document.querySelector("#correctR")
 
-let secondsLeft = 1;
-let score = 0;
-let currentQuest = 0;
-
-// const questionsData = require('../../mockup/mockQseed.json');
-
-
-
-// await fetch("../../mockup/mockQseed.json")
-//   .then(questionJSON => questionJSON.json())
-//   .then(questionData => questions = questionData);
-//   .then(questionData => questQuest(questionData));
-//   .then(questionData => console.log(questionData));
-
-questQuest()
-
-async function questQuest () {
-
-    await fetch("../../mockup/mockQseed.json")
-    .then(questionJSON => questionJSON.json())
-    .then(questionData => questi = questionData);
-
-    console.log(questi)
-    
-    return questions = questi
-}
-
-// console.log(questionsData)
-
-
-// let questions = [
-  
-//   {
-//     question: "What's my favorite color?", 
-//     answers:[
-//       { text: "Green", correct: false},
-//       { text: "Blue", correct: false},
-//       { text: "Red", correct: true},
-//       { text: "Black", correct: false},
-//     ]},
-//   {
-//     question: "What's my favorite band?", 
-//     answers:[
-//       { text: "Vampire Weekend", correct: true},
-//       { text: "The Strokes", correct: false},
-//       { text: "Glass Animals", correct: false},
-//       { text: "Bombay Bicyle Club", correct: false},
-//     ]},
-//   {
-//     question: "What is my birthday? 🤨", 
-//     answers:[
-//       { text: "August 20", correct: false},
-//       { text: "August 21", correct: true},
-//       { text: "August 22", correct: false},
-//       { text: "August 24", correct: false},
-//     ]},
-//   {
-//     question: "What's my favorite food?", 
-//     answers:[
-//       { text: "Gyros", correct: false},
-//       { text: "Pizza", correct: false},
-//       { text: "Tacos", correct: true},
-//       { text: "Sushi", correct: false},
-//     ]},
-//   {
-//     question: "What's my dream vacation?", 
-//     answers:[
-//       { text: "Athens", correct: false},
-//       { text: "Iceland", correct: false},
-//       { text: "Tokyo", correct: true},
-//       { text: "Fiji", correct: false},
-//     ]},
-//   {
-//     question: "Which TV Show would I pick if I could only watch one for the rest of my life?", 
-//     answers:[
-//       { text: "Midnight Gospel", correct: false},
-//       { text: "Game Of Thrones", correct: false},
-//       { text: "Parks and Rec", correct: true},
-//       { text: "The Office", correct: false},
-//     ]},
-  
-// ]
-
-
-// console.log(questions)
-
-setTime();
 
 startBtnEl.addEventListener("click", function(){
-  secondsLeft = 1;
   removeStartBtn();
   timeBtnEl.setAttribute("style", "display: flex")
   startGame();
@@ -114,25 +26,9 @@ startBtnEl.addEventListener("click", function(){
 
 function startGame() {
   questContainEl.setAttribute("style", "display: flex");
-//   renderQuestions();
+  timerEl.textContent = "Quiz Name";
 }
-
-function setTime() {
-  let timerInterval = setInterval(function() {
-    if(secondsLeft > 0){
-      secondsLeft++;
-    } else {
-      endGame();
-    }
-    timerEl.textContent = secondsLeft + " seconds";
-  }, 1000);
-  if(secondsLeft <= 0) {
-    clearInterval(timerInterval);
-    endGame();
-    return;
-  }
-} 
-
+ 
 function removeStartBtn() {
   startBtnEl.setAttribute("style", "display:none")
 //   instructEl.setAttribute("style", "display:none")
@@ -212,7 +108,6 @@ function endGame() {
 
 HighSBtnEl.addEventListener("click", function(event) {
   event.preventDefault();
-  secondsLeft = 0
   removeStartBtn();
   endGame();
 })
@@ -244,7 +139,7 @@ function storeHighscores() {
 //   localStorage.setItem("highscores", JSON.stringify(highscores));
 }
 
-saveQuestBtnEl.addEventListener("click", function(event) {
+saveQuestBtnEl.addEventListener("click", async function(event) {
   event.preventDefault();
   
   let question = questInputEl.value.trim();
@@ -253,13 +148,37 @@ saveQuestBtnEl.addEventListener("click", function(event) {
   let response_3 = answerInputCEl.value.trim();
   let response_4 = answerInputDEl.value.trim();
   let correct_response = correctRInputEl.value
+  // let quiz_id = parseInt(document.location.pathname.split('/')[document.location.pathname.split('/').length-1])
+  let quiz_id = 1
+
   
   
-  workingQuestions.push({question, response_1, response_2, response_3, response_4, correct_response});
+  workingQuestions.push({question, response_1, response_2, response_3, response_4, correct_response, quiz_id});
   console.log(workingQuestions)
   
   storeHighscores();
   renderHighScores();
+  if (question && response_1 && response_2 && response_3 && response_4 && correct_response && quiz_id) {
+    const response = await fetch(`/api/question/`, {
+      method: 'POST',
+      body: JSON.stringify({question, response_1, response_2, response_3, response_4, correct_response, quiz_id}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      // document.location.replace('/profile');
+      console.log(question)
+    } else {
+      alert('Failed to create project');
+    }
+  }
+
+
+
+
+
 });
 
 clearHSBtnEl.addEventListener("click", function(event){
