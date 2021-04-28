941823e2ed54472a3b08880d7f0bb1a7c521316c
Eric Hay <ericdanielhay@yahoo.com>
2021-04-28 11:54:22 -0500

    Updated index.js and questionRoutes.js.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const quizRoutes = require('./projectRoutes');
+const quizRoutes = require('./quizRoutes');
 
 router.use('/users', userRoutes);
 router.use('/quiz', quizRoutes);
