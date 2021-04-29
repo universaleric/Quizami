38944ab55bc01b0f75d03722bf1affddbacc5e2f
Julian Almendarez <ppalmendarez@yahoo.com>
2021-04-29 09:32:02 -0500

    model index code structured

--- models/index.js
+++ models/index.js
@@ -3,15 +3,24 @@ const Quiz = require('./Quiz');
 const Score = require('./Score');
 const User = require('./User');
 
-// User.hasMany(Project, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+Question.belongsTo(Quiz,{
+foreignKey: "quiz_id"
+});
 
-// Project.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+Quiz.hasMany(Question,{
+foreignKey: "quiz_id"
+});
+
+User.hasMany(Quiz, {
+foreignKey: "user_id"
+});
+
+Score.belongsTo(Quiz,{
+    foreignKey: "quiz_id", 
+});
+Score.belongsTo(User,{
+    foreignKey: "user_id", 
+});
+
 
 module.exports = { Question, Quiz, Score, User };
-
-//havent added "project"
